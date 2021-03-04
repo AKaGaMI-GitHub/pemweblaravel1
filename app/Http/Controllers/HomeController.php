079b19a95d45eb59b1d147dee436382cc0b47a27
unknown <netsahabat08@gmail.com>
2021-03-05 00:04:01 +0800

    pemweb laravel 1

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        $title="Data Mahasiswa";
+        $data['mahasiswa']=array(
+            'nim'=>'1905021007',
+            'nama'=>'Made Anantha Yoga',
+            'alamat'=>'Jalan Lingga No 2',
+            'nohp'=>'082146855989',
+            'email'=>'anantha@undiksha.ac.id'
+        );
+        return view('admin.beranda', compact('title','data'));
+    }
+    public function dashboard(){
+        $title="Data Dashboard";
+
+        return view('admin.dashboard', compact('title'));
+    }
+}
